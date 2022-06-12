8935a550a4f9b1ff0f8bbe8e48e272dc92b227f5
thekopiman <34156149+thekopiman@users.noreply.github.com>
2022-06-12 17:08:46 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import LoginPage from './screens/LoginPage';
+import LoginScreen from "./screens/Login";
+import HomePage from "./screens/HomePage";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <LoginPage />;
+  return (
+    <>
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Login Screen"
+            component={LoginScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Home Page"
+            component={HomePage}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
 }
-
-const styles = StyleSheet.create({});
